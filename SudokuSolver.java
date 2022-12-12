6d489ea6c0f891cd409d1a3c55e5bd0951620f08
logan-pipes <ldpipes@mta.ca>
2022-12-11 22:22:40 -0400

    Add forgotten parameter
    Oops!

--- SudokuSolver.java
+++ SudokuSolver.java
@@ -11,6 +11,7 @@ public abstract class SudokuSolver {
 	/**
 	 * Return a solved instance of the specified Sudoku puzzle.
 	 *
+	 * @param	s						the Sudoku to solve
 	 * @return							a solved Sudoku object (that is, one whose <code>isSolved</code> method returns <code>true</code>)
 	 * 									or null if no such solution exists
 	 * @throws	InstantiationException	if an error occurs when instantiating the solution Sudoku
